5b140e0d9c3d6afb506448dab2e853d5b7cecfa1
Sally Vu <88215189+HauVu-flinders@users.noreply.github.com>
2024-04-14 07:08:59 +0930

    Initial commit

---
+++ RazorPagesMovie/Models/Quiz.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RazorPagesQuiz.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
